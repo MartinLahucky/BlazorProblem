66d34768203ab126a5f0aebc2e8e4da8ca03d6c2
Martin Lahučký <martin@Martins-MacBook-Pro.local>
2023-02-07 13:53:19 +0100

    Initial commit

---
+++ BlazorProblem/Server/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using BlazorProblem.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorProblem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Description = "Something",
+                ImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png",
+                Price = 12.99m,
+                Title = "Just The Test"
+            }
+        };
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            return Ok(Products);
+        }
+    }
+}
+
